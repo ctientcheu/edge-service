7820ef38ad40195477a36f6be59d0967af251aca
Clement Tientcheu <clement.tientcheu@cerebrau.com>
2024-04-11 21:36:55 -0400

    end section 9.4

--- src/test/java/com/polarbookshop/edgeservice/EdgeServiceApplicationTests.java
+++ src/test/java/com/polarbookshop/edgeservice/EdgeServiceApplicationTests.java
@@ -2,12 +2,12 @@ package com.polarbookshop.edgeservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+@Testcontainers
+@SpringBootTest(classes = TestEdgeServiceApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class EdgeServiceApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
-
+	void contextLoads() {}
 }
---
+++ src/test/java/com/polarbookshop/edgeservice/TestEdgeServiceApplication.java
@@ -0,0 +1,26 @@
+package com.polarbookshop.edgeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.GenericContainer;
+
+/**
+ * @author clement.tientcheu@cerebrau.com
+ * @project edge-service
+ * @org Cerebrau
+ */
+@TestConfiguration(proxyBeanMethods = false)
+public class TestEdgeServiceApplication {
+
+    @Bean
+    @ServiceConnection(name = "redis")
+    GenericContainer<?> redisContainer() {
+        return new GenericContainer<>("redis:7.2.4").withExposedPorts(6379);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.from(EdgeServiceApplication::main).with(TestEdgeServiceApplication.class).run(args);
+    }
+}
