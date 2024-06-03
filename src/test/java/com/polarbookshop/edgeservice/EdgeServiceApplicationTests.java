54423e05badbdde17636aea32ea420b1f75d2c66
Clement Tientcheu <clement.tientcheu@cerebrau.com>
2024-06-02 23:39:32 -0400

    fix test failure

--- src/test/java/com/polarbookshop/edgeservice/EdgeServiceApplicationTests.java
+++ src/test/java/com/polarbookshop/edgeservice/EdgeServiceApplicationTests.java
@@ -2,13 +2,25 @@ package com.polarbookshop.edgeservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
-@SpringBootTest(
-    classes = TestEdgeServiceApplication.class,
-    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class EdgeServiceApplicationTests {
+
+  @MockBean
+  ReactiveClientRegistrationRepository clientRegistrationRepository;
+
+  @Bean
+  @ServiceConnection(name = "redis")
+  GenericContainer<?> redisContainer() {
+    return new GenericContainer<>("redis:7.2.4").withExposedPorts(6379);
+  }
 
   @Test
   void contextLoads() {}
